cc20368f3d09458b6ebad62f3d56d10e6e862e78
Noam <noamg@cox.net>
2016-03-01 21:01:07 -0500

    ng_todos

--- public/js/app.js
+++ public/js/app.js
@@ -17,9 +17,15 @@ app.controller('TodoController', function($scope){
     var index = $scope.todoList.indexOf(singleTodo);
     $scope.todoList.splice(index, 1);
   }
-  
+
   $scope.editTodo = function(singleTodo) {
-    $scope.currentTodo = singleTodo;
-    $scope.removeTodo(singleTodo);
+    var index = $scope.todoList.indexOf(singleTodo);
+    $scope.todoList.splice(index, 1,$scope.currentTodo);
+    $scope.currentTodo = '';
+  }
+  $scope.moveUp = function(singleTodo) {
+    var index = $scope.todoList.indexOf(singleTodo);
+    $scope.todoList.splice(index, 1);
+    $scope.todoList.splice(index-1, 0, singleTodo);
   }
 });
