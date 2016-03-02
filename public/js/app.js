ffc442de85e52ccdfbca45128621947a5092ef43
Noam <noamg@cox.net>
2016-03-01 20:42:52 -0500

    todo

--- public/js/app.js
+++ public/js/app.js
@@ -2,5 +2,24 @@ var app = angular.module('ngTodo', []);
 
 
 app.controller('TodoController', function($scope){
-  $scope.message = "I'm working ....";
+  $scope.todoList = ['rake the yard',
+                    'take a nap',
+                    'take another nap',
+                    'walk the dog',
+                    'eat the leftover meatloaf'];
+  $scope.saveTodo = function(){
+    var todoInfo = $scope.currentTodo;
+    $scope.todoList.push(todoInfo);
+    $scope.currentTodo = "";
+  }
+
+  $scope.removeTodo = function(singleTodo){
+    var index = $scope.todoList.indexOf(singleTodo);
+    $scope.todoList.splice(index, 1);
+  }
+  
+  $scope.editTodo = function(singleTodo) {
+    $scope.currentTodo = singleTodo;
+    $scope.removeTodo(singleTodo);
+  }
 });
